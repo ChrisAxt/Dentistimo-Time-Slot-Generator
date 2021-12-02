503dec0afecee1e1a7f579b84565d6653f82bbd2
axtc <axtc@student.chalmers.se>
2021-12-02 14:53:22 +0100

    fixed and tested genertion of time slots with mock data

--- time_slot_controller.js
+++ time_slot_controller.js
@@ -1,6 +1,6 @@
-require('dotenv').config()
-const mongoose = require("mongoose");
-mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@dentistimo0.vd9sq.mongodb.net/Dentistimo`);
+//require('dotenv').config()
+//const mongoose = require("mongoose");
+//mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@dentistimo0.vd9sq.mongodb.net/Dentistimo`);
 //var router = express.Router();
 var TimeSlot = require('./models/timeSlot')
 //var Clinic = require('../models/clinic'); // try import model from clinic handler
@@ -22,7 +22,6 @@ const publishTopic = '/Team5/Dentistimo/TimeSlots'
  */
 const client = mqtt.connect(HOST) //Change the parameter between HOST or LOCALHOST if you want to connect to the mosquitto test broker or a local broker. For local, mosquitto needs to be installed and running
 
-
 // Global variables 
 var date;
 var day;
@@ -30,7 +29,7 @@ var startHour
 var startMin
 var endHour
 var endMin
-var clinic;
+var clinicId;
 var dentistNo
 
 // MQTT
@@ -38,15 +37,19 @@ client.on('connect', function() {
     console.log("Connected to Mqtt broker successfully" )
     client.subscribe(subscribeTopic, function (err) {
         if (!err) {
-           console.log("Subscribed to " + subscribeTopic + " successfully")
+            console.log("Subscribed to " + subscribeTopic + " successfully")
+            client.publish(subscribeTopic, 'July 21, 1983 01:15:00-clinicName', {qos:1} )
         }else{
             console.log(err.message);
         }
     })
 })
 
-client.on('message', (topic, payload) => {
-  console.log('Received Message:', topic, payload.toString())
+
+
+client.on('message', (subscribeTopic, payload) => {
+  console.log('Received Message:', subscribeTopic, payload.toString())
+  //TODO - add error handling to below methods 
   readMessage(payload.toString)
   generateTimeSlots()
 })
@@ -56,28 +59,42 @@ client.on('message', (topic, payload) => {
 //**********************************************************************************************************************************/
 
 function readMessage (message) {
-    message = 'July 21, 1983 01:15:00-clinicName' // = example
+    message = '02 Dec 2021 09:32:00 GMT+1-clinicName' // = example
  
-    var dateArray;
-    var clinicArray;
+    var dateArray = new Array;
+    var clinicArray = new Array;
 
-    let n = message.search("-");
+    const n = message.search("-");
+    console.log('Split: ' + n)
     let messageArray = message.split('')
+    //console.log('Message Array: ' + messageArray)
 
-    for(let i = 0; i < messageArray.length; i++){
+
+    const messageLength = messageArray.length
+
+    for(let i = 0; i < messageLength; i++){
+        //console.log('Message array length: ' + n)
         if(i < n){
-            dateArray[i] = messageArray[i]
-            if(i = n-1){
+            //console.log(messageArray[i])
+            dateArray.push(messageArray[i]) 
+            //console.log(dateArray[i])
+            /* if(i = n-1){
                 date = new Date(dateArray.toString)
                 day = date.getDay()
-            }
+            } */
         }else if(i > n){
-            clinicArray[i] = messageArray[i]
-            if(i = messageArray.length - 1){
-                clinic = clinicArray.toString
-            }
-        }
+            clinicArray.push(messageArray[i])
+            /* if(i = messageArray.length - 1){
+                clinicId = clinicArray.toString
+            } */ 
+        } 
     }
+    date = new Date(Date.parse(dateArray.join("")))
+    var weekday = new Array("sunday", "monday", "tuesday", "wednesday",
+                    "thursday", "friday", "saturday");
+
+    day = weekday[date.getDay()];
+    clinicId = clinicArray.toString()
 }
 
 //**********************************************************************************************************************************/
@@ -89,18 +106,16 @@ function generateTimeSlots() {
     var dayOpeningHours = '9:00-15:00' // = example
     dentistNo = 3 // = example
 
-    router.get('/api/users/clinicName/:name/openingHours', function (req, res, next){
-
-        Clinic.findOne({clinicName: req.params.name}, function(err, user){
+       /*  Clinic.findOne({clinicId: clinicId}, function(err, clinic){
             if (err) { 
-                console.log(clinicName)
+                console.log(clinicId)
                 res.status(500).json({"message": "get failed"}); 
                 return next(err);
             }
             if (clinic == null) {
                 res.status(404).json({"message": "clinic not found"});
             }
-            res.status(200).json(clinic);
+            console.log(clinic)
             var openingHours = clinic.openingHours
             // Access amount of dentists for specific clinic from database
             dentistNo = clinic.dentists
@@ -123,29 +138,32 @@ function generateTimeSlots() {
               break;
             default:   
         }
-    });
+    }); */
      
-})
+
     
     // Split the dayOpeningHours into 2 times then into min and hours
     splitTime(dayOpeningHours);
     
     //calculate how many 1 hour slots are in this day
     var OHinMin = ((endHour * 60) + endMin) - ((startHour * 60) + startMin)
-    var slotNo = OHinMin / 30
-    var timeSlots;
+    console.log(endHour)
+    var slotNo = (OHinMin / 30) - 1
+    console.log(slotNo)
+    var timeSlots = new Array
     // generate timeslots based on above
     //create slots in loop from opening hour until ammout of slots is reached
     for(let i = 0; i <= slotNo; i++){
         var timeSlot = new TimeSlot();
         timeSlot.start = convertStartTime(i, startHour, startMin)
-        timeSlot.end = convertEndTime(i, endHour, endMin)
-        timeSlot.available = this.dentistNo
-        timeSlot.date = this.date
+        timeSlot.end = convertEndTime(i, startHour, startMin)
+        timeSlot.available = dentistNo
+        timeSlot.date = date.toDateString()
+        console.log(timeSlot)
         
-        timeSlot[i] = this.timeSlot;
+        timeSlots.push(timeSlot)
     };
-    console.log(timeSlots)
+    console.log('Time Slots : ' + timeSlots)
 }  
 
 //**********************************************************************************************************************************/
@@ -153,54 +171,67 @@ function generateTimeSlots() {
 //**********************************************************************************************************************************/
 
 function splitTime (dayOpeningHours){
-
+    console.log('inside split time')
     let n = dayOpeningHours.search("-");
+    console.log(n)
     let dayOpeningHoursArray = dayOpeningHours.split('')
-    var startTimeArray
-    var endTimeArray
-    var startHourArray
-    var startMinArray
-    var endHourArray
-    var endMinArray
+    var startTimeArray = new Array
+    var endTimeArray = new Array
+    var startHourArray = new Array
+    var startMinArray = new Array
+    var endHourArray = new Array
+    var endMinArray = new Array
 
     for(let i = 0; i < dayOpeningHoursArray.length; i++){
         if(i < n){
-            startTimeArray[i] = dayOpeningHoursArray[i]
+            startTimeArray.push(dayOpeningHoursArray[i])
         }else if(i > n){
-            endTimeArray[i] = dayOpeningHoursArray[i]
+            endTimeArray.push(dayOpeningHoursArray[i])
         }
     }
-    const startSplit = startTimeArray.find(element => element == ':');
-    const endSplit = endTimeArray.find(element => element == ':');
+    const startSplit = startTimeArray.join("").search(":")
+    console.log(startSplit)
+    //const startSplit = startTimeArray.find(element => element == ':');
+    const endSplit = endTimeArray.join("").search(":")
 
     //Split opening time into hour/min
     for(let i = 0; i < startTimeArray.length; i++){
         if(i < startSplit){
-            startHourArray[i] = startTimeArray[i]
-            if(i = startTimeArray.length -1){
+            startHourArray.push(startTimeArray[i])
+/*             if(i = startTimeArray.length -1){
                 startHour = parseInt(startHourArray.toString)
-            }
+                console.log('start hour: ' + startHour.toString())
+            } */
         }else if(i > startSplit){
-            startMinArray[i] = startTimeArray[i]
-            if(i = startTimeArray.length - 1){
+            startMinArray.push(startTimeArray[i])
+/*             if(i = startTimeArray.length - 1){
                 startMin = parseInt(startMinArray.toString)
-            }
+                console.log('start min int parse :' + startMin)
+                console.log('start hour: ' + startMin.toString())
+            } */
         }
     }
+    startHour = parseInt(startHourArray.toString())
+    startMin = parseInt(startMinArray.join(""))
     //Split closing time into hour/min
     for(let i = 0; i < endTimeArray.length; i++){
         if(i < endSplit){
-            endHourArray[i] = endTimeArray[i]
-            if(i = endTimeArray.length -1){
+            endHourArray.push(endTimeArray[i]) 
+            
+/*             if(i = endTimeArray.length -1){
                 endHour = parseInt(endHourArray.toString)
-            }
+                console.log('end hour: ' + endHour.toString())
+            } */
         }else if(i > endSplit){
-            endMinArray[i] = endTimeArray[i]
-            if(i = endTimeArray.length - 1){
+            endMinArray.push(endTimeArray[i])
+/*             if(i = endTimeArray.length - 1){
                 endMin = parseInt(endMinArray.toString)
-            }
+                console.log('end min: ' + endMin.toString())
+            } */
         }
     }
+    endHour = parseInt(endHourArray.join(""))
+    endMin = parseInt(endMinArray.join(""))
 }
 
 //**********************************************************************************************************************************/
@@ -209,15 +240,23 @@ function splitTime (dayOpeningHours){
 
 function convertStartTime(i, startHour, startMin){
 
-    startMin = startMin + (30 * (i + 1))
+    startMin = startMin + (30 * i)
+
     var startTime
 
     if(startMin > 59){
-        Math.floor(startHour =+ (startMin / 60)) 
-        startMin = startMin - Math.floor(startMin / 60)
+        startMin = startMin / 60
+        startHour += Math.floor(startMin) 
+        var n = Math.trunc(startMin)
+        startMin = (startMin) - n
+        
         startMin = (((startMin / 100) * 60) * 100)
     }
-    return startTime = startHour.toString + startMin.toString  
+    var trailingZero = ''
+        if(startMin == 0){
+            trailingZero = '0'
+        }
+    return startTime = startHour.toString() +':'+ startMin.toString() + trailingZero  
 }
 
 //**********************************************************************************************************************************/
@@ -230,11 +269,18 @@ function convertEndTime(i, endHour, endMin){
     var startTime
 
     if(endMin > 59){
-        Math.floor(endHour =+ (endMin / 60)) 
-        endMin = endMin - Math.floor(endMin / 60)
+        endMin = endMin / 60
+        endHour += Math.floor(endMin) 
+        var n = Math.trunc(endMin)
+        endMin = (endMin) - n
+        
         endMin = (((endMin / 100) * 60) * 100)
     }
-    return startTime = endHour.toString + endMin.toString  
+    var trailingZero = ''
+    if(endMin == 0){
+        trailingZero = '0'
+    }
+    return startTime = endHour.toString() +':'+ endMin.toString() + trailingZero
 }
 
 //**********************************************************************************************************************************/
@@ -244,5 +290,3 @@ function convertEndTime(i, endHour, endMin){
     //**Optional*/
     // For each appointment for the clinic, if there is an apponintment with the same start time as a 
     // time slot, subtract 1 from the appointments in the time slot for each appointment that matches the start time
-    
-
